9516b2ca1f4584c629d6089e31526959cb9e82a8
Miriam Esteve <miriam.estevec@umh.es>
2021-02-16 12:23:45 +0100

    scores DynamicFDH and DynamicEAT IdeltaK

--- eat/scores.py
+++ eat/scores.py
@@ -33,7 +33,7 @@ class EXIT(Exception):
 
 class Scores:
     def __init__(self, matrix, x, y, tree):
-        self.matrix = matrix.loc[:, x + y]  # Order variables
+        self.matrix = matrix.loc[:, x + y].copy()  # Order variables
         self.N = len(self.matrix)
         self._check_columnsX_in_data(matrix, x)
 
